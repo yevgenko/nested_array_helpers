11921f159e5aaa968c4ee90fb0ef65e3bfbe1853
Yevhen V <wik@bdhr.co>
2017-05-09 23:24:46 +0100

    Add test helper

--- test/nested_array_helpers_test.rb
+++ test/nested_array_helpers_test.rb
@@ -1,6 +1,4 @@
-gem 'minitest', '~> 5.4'
-require 'minitest/autorun'
-require_relative '../lib/nested_array_helpers'
+require 'test_helper'
 
 module NestedArrayHelpersTest
   class SortNestedArrayTest < Minitest::Test
---
+++ test/test_helper.rb
@@ -0,0 +1,3 @@
+gem 'minitest', '~> 5.4'
+require 'minitest/autorun'
+require_relative '../lib/nested_array_helpers'
