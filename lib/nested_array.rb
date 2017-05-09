c3c4b1385bc5a9bd2a3c19945c42c729af1c353b
Yevhen V <wik@bdhr.co>
2017-05-09 22:10:38 +0100

    Remove NestedArray class

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,18 +1,3 @@
-class NestedArray < Array
-  def sort_nested_array(node_key, nested_array_key)
-    self.class.new self.map { |item|
-      item[node_key].sort_by! { |nested_item| nested_item[nested_array_key] }
-      item
-    }
-  end
-
-  def sort_by_nested_array(node_key, nested_array_key)
-    self.class.new self.sort_by { |item|
-      item[node_key].map{ |nested_item| nested_item[nested_array_key] }
-    }
-  end
-end
-
 module NestedArrayHelpers
   def sort_nested_array(node_key, nested_array_key)
     self.map { |item|
