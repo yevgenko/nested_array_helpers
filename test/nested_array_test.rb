edc974a638c26c5205fdf34ae132245b6049f19b
Yevhen V <wik@bdhr.co>
2017-05-09 22:08:31 +0100

    Replace nested array class with module helper

--- test/nested_array_test.rb
+++ test/nested_array_test.rb
@@ -3,21 +3,14 @@ require 'minitest/autorun'
 require_relative '../lib/nested_array'
 
 class NestedArrayTest < Minitest::Test
-  def test_empty_array_hash
-    assert_equal [], NestedArray.new
-  end
-
-  def test_non_empty_array_hash
-    assert_equal [{id: 1}], NestedArray.new([{id: 1}])
-  end
-
   def test_sort_node_by_sku
     expected = [
       { id: 1, products: [ { sku: 'a' }, { sku: 'b' }, { sku: 'c' } ] }
     ]
-    a = NestedArray.new [
+    a = [
       { id: 1, products: [ { sku: 'c' }, { sku: 'a' }, { sku: 'b' } ] }
     ]
+    a.extend(NestedArrayHelpers)
 
     assert_equal expected, a.sort_nested_array(:products, :sku)
   end
@@ -26,9 +19,10 @@ class NestedArrayTest < Minitest::Test
     expected = [
       { id: 1, products: [ { title: 'a' }, { title: 'b' }, { title: 'c' } ] }
     ]
-    a = NestedArray.new [
+    a = [
       { id: 1, products: [ { title: 'c' }, { title: 'a' }, { title: 'b' } ] }
     ]
+    a.extend(NestedArrayHelpers)
 
     assert_equal expected, a.sort_nested_array(:products, :title)
   end
@@ -37,9 +31,10 @@ class NestedArrayTest < Minitest::Test
     expected = [
       { id: 1, line_items: [ { title: 'a' }, { title: 'b' }, { title: 'c' } ] }
     ]
-    a = NestedArray.new [
+    a = [
       { id: 1, line_items: [ { title: 'c' }, { title: 'a' }, { title: 'b' } ] }
     ]
+    a.extend(NestedArrayHelpers)
 
     assert_equal expected, a.sort_nested_array(:line_items, :title)
   end
@@ -49,7 +44,7 @@ class NestedArrayTest < Minitest::Test
       { id: 1, products: [ { sku: 'c' } ] },
       { id: 2, products: [ { sku: 'b' } ] },
       { id: 3, products: [ { sku: 'a' } ] }
-    ]
+    ].extend(NestedArrayHelpers)
   end
 
   def test_without_sort_by
@@ -59,7 +54,7 @@ class NestedArrayTest < Minitest::Test
 
   def test_sort_by_sku
     expected = [3, 2, 1]
-    assert_equal expected, NestedArray.new(orders).sort_by_nested_array(:products, :sku).map{ |o| o[:id] }
+    assert_equal expected, orders.sort_by_nested_array(:products, :sku).map{ |o| o[:id] }
   end
 
   def multi_products_orders
@@ -67,12 +62,12 @@ class NestedArrayTest < Minitest::Test
       { id: 1, products: [ { sku: 'c' }, { sku: 'c' } ] },
       { id: 2, products: [ { sku: 'b' }, { sku: 'b' } ] },
       { id: 3, products: [ { sku: 'a' }, { sku: 'a' } ] }
-    ]
+    ].extend(NestedArrayHelpers)
   end
 
   def test_multi_products_sort_by_sku
     expected = [3, 2, 1]
-    assert_equal expected, NestedArray.new(multi_products_orders).sort_by_nested_array(:products, :sku).map{ |o| o[:id] }
+    assert_equal expected, multi_products_orders.sort_by_nested_array(:products, :sku).map{ |o| o[:id] }
   end
 
   def tricky_orders
@@ -80,12 +75,12 @@ class NestedArrayTest < Minitest::Test
       { id: 1, products: [ { sku: 'c' }, { sku: 'a' }, { sku: 'c' } ] },
       { id: 2, products: [ { sku: 'a' }, { sku: 'c' }, { sku: 'a' } ] },
       { id: 3, products: [ { sku: 'b' }, { sku: 'b' }, { sku: 'b' } ] }
-    ]
+    ].extend(NestedArrayHelpers)
   end
 
   def test_tricky_orders_sort_by_sku
     expected = [2, 3, 1]
-    assert_equal expected, NestedArray.new(tricky_orders).sort_by_nested_array(:products, :sku).map{ |o| o[:id] }
+    assert_equal expected, tricky_orders.sort_by_nested_array(:products, :sku).map{ |o| o[:id] }
   end
 
   def book_orders
@@ -93,12 +88,12 @@ class NestedArrayTest < Minitest::Test
       { id: 1, books: [ { isbn: 'c' }, { isbn: 'a' }, { isbn: 'c' } ] },
       { id: 2, books: [ { isbn: 'a' }, { isbn: 'c' }, { isbn: 'a' } ] },
       { id: 3, books: [ { isbn: 'b' }, { isbn: 'b' }, { isbn: 'b' } ] }
-    ]
+    ].extend(NestedArrayHelpers)
   end
 
   def test_another_array_sort_by_nested_array
     expected = [2, 3, 1]
-    assert_equal expected, NestedArray.new(book_orders).sort_by_nested_array(:books, :isbn).map{ |o| o[:id] }
+    assert_equal expected, book_orders.sort_by_nested_array(:books, :isbn).map{ |o| o[:id] }
   end
 
   module Foobar
