09d1d41cc5055f0648fe6c2aa58700ba51dd6bcb
Yevhen V <wik@bdhr.co>
2017-05-09 23:34:59 +0100

    Refactor test for chaining helpers

--- test/nested_array_helpers_test.rb
+++ test/nested_array_helpers_test.rb
@@ -1,25 +1,23 @@
 require 'test_helper'
 
-module NestedArrayHelpersTest
-  class ChainHelpersTest < Minitest::Test
-    def tricky_orders
-      [
-        { id: 1, products: [ { sku: 'c' }, { sku: 'a' }, { sku: 'c' } ] },
-        { id: 2, products: [ { sku: 'a' }, { sku: 'c' }, { sku: 'a' } ] },
-        { id: 3, products: [ { sku: 'b' }, { sku: 'b' }, { sku: 'b' } ] }
-      ].extend(NestedArrayHelpers)
-    end
+class ChainingHelpersTest < Minitest::Test
+  def tricky_orders
+    [
+      { id: 1, products: [ { sku: 'c' }, { sku: 'a' }, { sku: 'c' } ] },
+      { id: 2, products: [ { sku: 'a' }, { sku: 'c' }, { sku: 'a' } ] },
+      { id: 3, products: [ { sku: 'b' }, { sku: 'b' }, { sku: 'b' } ] }
+    ].extend(NestedArrayHelpers)
+  end
 
-    def test_sort_nested_array_and_sort_by_nested_array
-      expected= [
-        { id: 2, products: [ { sku: 'a' }, { sku: 'a' }, { sku: 'c' } ] },
-        { id: 1, products: [ { sku: 'a' }, { sku: 'c' }, { sku: 'c' } ] },
-        { id: 3, products: [ { sku: 'b' }, { sku: 'b' }, { sku: 'b' } ] }
-      ]
+  def test_sort_nested_array_and_sort_by_nested_array
+    expected= [
+      { id: 2, products: [ { sku: 'a' }, { sku: 'a' }, { sku: 'c' } ] },
+      { id: 1, products: [ { sku: 'a' }, { sku: 'c' }, { sku: 'c' } ] },
+      { id: 3, products: [ { sku: 'b' }, { sku: 'b' }, { sku: 'b' } ] }
+    ]
 
-      assert_equal expected, tricky_orders.
-        sort_nested_array(:products, :sku).
-        sort_by_nested_array(:products, :sku)
-    end
+    assert_equal expected, tricky_orders.
+      sort_nested_array(:products, :sku).
+      sort_by_nested_array(:products, :sku)
   end
 end
